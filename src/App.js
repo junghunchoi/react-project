92aef506d7487917a1fb1dba2c79ae590c4afbdc
junghunchoi <azzudi@naver.com>
2022-09-01 23:44:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,28 @@
- import logo from './logo.svg';
- import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
-
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import ListBoardComponent from "./view/listboard/ListBoardComponent";
+import HeaderComponent from "./view/component/HeaderComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [hello, setHello] = useState([])
-
-  useEffect(() => {
-    axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-  }, []);
-
   return (
-
-      <div className="App-header">
-          <header className="App-header">
-              <ul>
-                  {hello.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-              </ul>
-
-
-          </header>
+    <Router >
+      <div className="Margin"> 
+      <HeaderComponent />
+      <nav className="navbar navbar-dark bg-dark">
+      <div>
+        <Link to="/board">게시판</Link>
+        <Link to="/">차트</Link>
+        <Link to="/">네이버지도</Link>
+        
       </div>
-
+      </nav>
+      <Routes>
+        
+        <Route path="/board" element={<ListBoardComponent />}></Route>
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
